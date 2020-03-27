438d1cf62c76a6061e6c24c32bcfdb68e0b912c9
Djenko09 <48559961+Djenko09@users.noreply.github.com>
2020-03-27 16:13:15 +0000

    first commit

---
+++ resources/js/components/home.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <div class="">
+    <h1 style="background-color:yellow">Hello</h1>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>

8e3779d3c2eed2bb9e643609295445bd7410abe8
paulocustodio10 <paulomvc10@gmail.com>
2020-03-27 16:49:14 +0000

    teste router v2

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,23 +8,7 @@ Vue.use(VueRouter);
 import homeComponent from "./components/homeComponent";
 const home = Vue.component('home', homeComponent);
 
-import loginComponent from "./components/loginComponent";
-const login = Vue.component('login', loginComponent);
-
-const routes = [
-    { path: '/', component: homeComponent },
-    { path: '/login', component: login, name: "login" }
-
-];
-
-const router = new VueRouter({
-    routes,
-
-});
-
-
  new Vue({
     el: "#app",
-    router
 
 });
--- resources/js/components/loginComponent.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1 class="card-title mb-4 mt-1 jumbotron ">isto é o login</h1>
-
-  </div>
-</templat

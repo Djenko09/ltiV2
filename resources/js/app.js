035feb8fa6c0dcc1017ccb5b4178ee2626b78b58
paulocustodio10 <paulomvc10@gmail.com>
2020-03-27 16:13:22 +0000

    setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import homeComponent from "./components/homeComponent";
+const home = Vue.component('home', homeComponent);
+
+ new Vue({
+    el: "#app",
+
+});

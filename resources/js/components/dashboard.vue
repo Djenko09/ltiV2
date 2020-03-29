c293a377bae12f30ea6daa6c4838d407216bd0ed
Djenko09 <48559961+Djenko09@users.noreply.github.com>
2020-03-29 16:41:37 +0100

    dashboard

---
+++ resources/js/components/dashboard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+
+    <div class="starter-template jumbotron bg-info">
+      <h1>ISTO É A PAGINA DPS DO LOGIN CRL</h1>
+      <a class="btn btn-primary"  v-on:click.prevent="getServers()">My servers</a>
+    </div>
+    <div class="">
+
+
+    </div>
+
+
+  </div>
+</template>
+
+<script type="text/javascript">
+
+export default {
+
+  data: function() {
+    return{
+      tokens:'',
+      url: process.env.MIX_URL,
+      };
+    },
+    methods: {
+        getServers(){
+          axios.get(this.url+'/compute/v2.1/servers', {
+            headers: {
+              Authorization: token
+            }
+
+          }).then(response=>{
+            console.log(response);
+          });
+
+    },
+    mounted() {
+      console.log(tokens)
+    }
+  }
+}
+</script>

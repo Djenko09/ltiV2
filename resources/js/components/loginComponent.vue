aabe9f75976b577edd40bde6352e9960cf3f66aa
paulocustodio10 <paulomvc10@gmail.com>
2020-03-29 16:37:02 +0100

    Token guardado + teste get (instancias)

--- resources/js/components/loginComponent.vue
+++ resources/js/components/loginComponent.vue
@@ -1,90 +1,119 @@
 <template>
   <div>
-    <h1 class="card-title mb-4 mt-1 jumbotron ">isto é o login</h1>
+    <h1 class="card-title mb-4 mt-1 jumbotron">isto é o login</h1>
 
-
-        <div class="starter-template jumbotron bg-info">
-          <strong>{{ message }}</strong>
-          <div class="container">
-            <h2>Login</h2>
-              <div class="form-group">
-                <label for="inputName">Username</label>
-                <input
-                type="text"
-                class="form-control"
-                v-model="user.name"
-                name="email"
-                id="inputEmail"
-                placeholder="Your username"
-                />
-              </div>
-              <div class="form-group">
-                <label for="inputPassword">Password</label>
-                <input
-                type="password"
-                class="form-control"
-                v-model="user.password"
-                name="password"
-                id="inputPassword"
-                />
-              </div>
-              <div class="form-group" text-align="center" >
-                <a class="btn btn-primary" v-on:click.prevent="login(user)">Login</a>
-              </div>
-            </div>
+    <div class="starter-template jumbotron bg-info">
+      <strong>{{ message }}</strong>
+      <div class="container">
+        <h2>Login</h2>
+        <div class="form-group">
+          <label for="inputName">Username</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="credentials.name"
+            name="email"
+            id="inputEmail"
+            placeholder="Your username"
+          />
         </div>
-
-
+        <div class="form-group">
+          <label for="inputPassword">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            v-model="credentials.password"
+            name="password"
+            id="inputPassword"
+          />
+        </div>
+        <div class="form-group" text-align="center">
+          <a class="btn btn-primary" v-on:click.prevent="login()">Login</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/javascript">
-
 export default {
-
   data: function() {
-    return{
+    return {
       //tokens:'',
-      user:{
-        name:null,
-        password:null,
+      user: {
+        token: null
+      },
+      credentials: {
+        name: null,
+        password: null
       },
       url: process.env.MIX_URL,
       showMessage: false,
       message: ""
-      };
-    },
-    methods: {
-        login(){
-          axios.post(this.url+'/identity/v3/auth/tokens', {
-            auth:{
-              identity:{
-                methods:[
-                  "password"
-                ],
-               password:{
-                 user:{
-                   name:this.user.name,
-                   domain: {
-                     name: "default"
-                   },
-                   password:this.user.password
-                 }
-               }
-             }
-           }
-          }).then(response=>{
-            this.$router.push("/token");
-          }).catch(error =>{
-            this.message = "Invalid Credentials";
-            this.showMessage = true;
-            console.log(error);
-          })
+    };
+  },
+  methods: {
+    login() {
+      axios
+        .post(
+          this.url + "/identity/v3/auth/tokens",
+          {
+            auth: {
+              identity: {
+                methods: ["password"],
+                password: {
+                  user: {
+                    name: this.credentials.name,
+                    domain: {
+                      name: "Default"
+                    },
+                    password: this.credentials.password
+                  }
+                },
+                scope: {
+                  project: {
+                    id: "811c3e6ac7fb443ab4e89bb54bf0ca77"
+                  }
+                }
+              }
+            }
+          },
+          {
+            headers: {
+              "Content-Type": "application/json"
+            }
+          }
+        )
+        .then(response => {
+          console.log(response);
+          this.user = response.data.token.user;
+          this.user.token = response.headers['x-subject-token'];
+          
+          console.log(this.user.token);
 
+           this.$store.commit("setToken", this.user.token); //guarda token
+
+           return axios.get(this.url + "/compute/v2.1/servers", {
+            headers: {'x-auth-token': this.user.token} })
+          .then(response=>{
+            console.log(response);
+          });
+        })
+          .then(response => {
+          this.$store.commit("setUser",  this.user);
+      
+
+          //this.$router.push("/instancias");
+        })
+        .catch(error => {
+          this.message = "Invalid Credentials";
+          this.showMessage = true;
+          console.log(error);
+        });
     },
     mounted() {
-      console.log(tokens)
+     
     }
   }
-}
+};
 </script>
---
+++ resources/js/vuex.js
@@ -0,0 +1,73 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist';
+
+Vue.use(Vuex);
+
+const vuexLocalStorage = new VuexPersist({
+    key: 'vuex',
+    storage: window.localStorage
+});
+
+export default  new Vuex.Store({
+
+    state: {
+        token: "",
+        user: null,
+       
+    },
+
+    getters: {
+        isAuthenticated: state => !state.token,
+        authStatus: state => state.status,
+      },
+
+    mutations: {
+        clearUserAndToken: (state) => {
+            state.user = null;
+            state.token = "";
+            window.localStorage.removeItem('user');
+            window.localStorage.removeItem('token');
+            axios.defaults.headers.common.Authorization = undefined;
+         
+
+        },
+        setToken: (state, token) => {
+            state.token = token;
+            window.localStorage.setItem('token', token);
+            axios.defaults.headers.common.Authorization = "Bearer " + token;
+        },
+        clearUser: state => {
+            state.user = null;
+            localStorage.removeItem("user");
+        },
+        clearToken: (state) => {
+            state.token = "";
+            window.localStorage.removeItem('token');
+            axios.defaults.headers.common.Authorization = undefined;
+        },
+
+        setUser: (state, user) => {
+            state.user = user;
+            window.localStorage.setItem('user', JSON.stringify(user));
+          
+        },
+        loadTokenAndUserFromSession: state => {
+            state.token = "";
+            state.user = null;
+            let token = localStorage.getItem("token");
+            let user = localStorage.getItem("user");
+            if (token) {
+                state.token = token;
+                axios.defaults.headers.common.Authorization = "Bearer " + token;
+            } else {
+                axios.defaults.headers.common.Authorization = undefined;
+            }
+            if (user) {
+                state.user = JSON.parse(user);
+            }
+        },
+    }
+});
+
+
